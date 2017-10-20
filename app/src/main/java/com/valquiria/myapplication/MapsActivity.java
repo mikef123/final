6b89b1d00884f8029ec56cbc6b0c37031a1bcdb9
Miguel Urquijo <miguelferu@gmail.com>
2017-10-20 17:33:52 -0500

    ruta

--- app/src/main/java/com/valquiria/myapplication/MapsActivity.java
+++ app/src/main/java/com/valquiria/myapplication/MapsActivity.java
@@ -1,28 +1,42 @@
 package com.valquiria.myapplication;
 
 import android.*;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -31,11 +45,26 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -45,6 +74,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private FirebaseAuth.AuthStateListener mAuthListener;
     EditText nombre;
     EditText correo;
+    EditText mAddress;
     String la;
     String lo;
     private static final int localizacion = 3;
@@ -82,6 +112,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         nombre.setText(user.getDisplayName());
         correo.setText(user.getEmail());
         mapFragment.getMapAsync(this);
+
+
+
+
     }
 
     protected	LocationRequest createLocationRequest()	 {
@@ -214,6 +248,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setZoomGesturesEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        final Geocoder mGeocoder = new Geocoder(getBaseContext());
+        mAddress = (EditText) findViewById(R.id.texto);
+        //set focus and show keyboard
+        mAddress.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+
+        mAddress.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == R.id.action_custom || actionId == EditorInfo.IME_ACTION_SEND ||  actionId == EditorInfo.IME_ACTION_UNSPECIFIED|| actionId == EditorInfo.IME_ACTION_GO || actionId == EditorInfo.IME_ACTION_DONE) {
+                    // hide keyboard
+                    InputMethodManager inputMethodManager = (InputMethodManager) mAddress.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(mAddress.getWindowToken(), 0);
+                    busqueda(mGeocoder);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+
+
     }
 
     @Override
@@ -233,5 +290,278 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //Abrir actividad para	configuración etc
         }
         return	super.onOptionsItemSelected(item);
+    }
+    public void busqueda(Geocoder mGeocoder)
+    {
+
+        //Cuando se realice la busqueda
+
+        String addressString = mAddress.getText().toString();
+        if (!addressString.isEmpty()) {
+            try {
+                List<Address> addresses = mGeocoder.getFromLocationName(
+                        addressString, 2,
+                        lowerLeftLatitude,
+                        lowerLeftLongitude,
+                        upperRightLatitude,
+                        upperRigthLongitude);
+                if (addresses != null && !addresses.isEmpty()) {
+                    Address addressResult = addresses.get(0);
+                    LatLng position = new LatLng(addressResult.getLatitude(), addressResult.getLongitude());
+                    if (mMap != null) {
+                        Marker javeriana1 = mMap.addMarker(new MarkerOptions().position(position).title("lllooo").icon(BitmapDescriptorFactory
+                                .fromResource(R.drawable.bike))
+                                .snippet("jajajaj") //Texto de información
+                                .alpha(0.5f)); //Transparencia);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+                        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                        mMap.getUiSettings().setCompassEnabled(true);
+                        mMap.getUiSettings().setZoomGesturesEnabled(true);
+                        mMap.getUiSettings().setZoomControlsEnabled(true);
+                        //-------------------------------------------------------------------------------------------------
+                        LatLng lugar1 = new LatLng(4.6272, -74.0639);
+                        Marker javeriana = mMap.addMarker(new MarkerOptions().position(lugar1).title("biciJaveriana").icon(BitmapDescriptorFactory
+                                .fromResource(R.drawable.bike))
+                                .snippet("Lunes a viernes : 8 am - 5pm") //Texto de información
+                                .alpha(0.5f)); //Transparencia);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(lugar1));
+                        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                        mMap.getUiSettings().setCompassEnabled(true);
+                        mMap.getUiSettings().setZoomGesturesEnabled(true);
+                        mMap.getUiSettings().setZoomControlsEnabled(true);
+                        MarkerOptions marcadorDestino= new MarkerOptions();
+                        marcadorDestino.position(position);
+                        marcadorDestino.title("Este es tu destino");
+                        //marcadorDestino.icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons("ic_marcador_destino",80,80)));
+                        mMap.addMarker(marcadorDestino);
+
+                        String url = obtenerDireccionesURL(javeriana.getPosition(), position);
+                        DownloadTask downloadTask = new DownloadTask();
+                        downloadTask.execute(url);
+
+
+
+
+
+                    }
+                } else {Toast.makeText(MapsActivity.this, "Dirección no encontrada", Toast.LENGTH_SHORT).show();}
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {Toast.makeText(MapsActivity.this, "La dirección esta vacía", Toast.LENGTH_SHORT).show();}
+
+
+    }
+
+
+
+    private String obtenerDireccionesURL(LatLng origin,LatLng dest){
+
+        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+
+        String sensor = "sensor=false";
+
+        String parameters = str_origin+"&"+str_dest+"&"+sensor;
+
+        String output = "json";
+
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+
+        return url;
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            String data = "";
+
+            try{
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("ERROR AL OBTENER ",e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            parserTask.execute(result);
+        }
+    }
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try{
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                routes = parser.parse(jObject);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            for(int i=0;i<result.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                List<HashMap<String, String>> path = result.get(i);
+
+                for(int j=0;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                lineOptions.addAll(points);
+                lineOptions.width(4);
+                lineOptions.color(Color.rgb(0,0,255));
+            }
+            if(lineOptions!=null) {
+                mMap.addPolyline(lineOptions);
+            }
+        }
+    }
+    public class DirectionsJSONParser {
+
+        public List<List<HashMap<String,String>>> parse(JSONObject jObject){
+
+            List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String,String>>>() ;
+            JSONArray jRoutes = null;
+            JSONArray jLegs = null;
+            JSONArray jSteps = null;
+
+            try {
+
+                jRoutes = jObject.getJSONArray("routes");
+
+                for(int i=0;i<jRoutes.length();i++){
+                    jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
+                    List path = new ArrayList<HashMap<String, String>>();
+
+                    for(int j=0;j<jLegs.length();j++){
+                        jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
+
+                        for(int k=0;k<jSteps.length();k++){
+                            String polyline = "";
+                            polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                            List<LatLng> list = decodePoly(polyline);
+
+                            for(int l=0;l<list.size();l++){
+                                HashMap<String, String> hm = new HashMap<String, String>();
+                                hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
+                                hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
+                                path.add(hm);
+                            }
+                        }
+                        routes.add(path);
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.toString();//printStackTrace();
+            }catch (Exception e){
+            }
+
+            return routes;
+        }
+
+        private List<LatLng> decodePoly(String encoded) {
+
+            List<LatLng> poly = new ArrayList<LatLng>();
+            int index = 0, len = encoded.length();
+            int lat = 0, lng = 0;
+
+            while (index < len) {
+                int b, shift = 0, result = 0;
+                do {
+                    b = encoded.charAt(index++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lat += dlat;
+
+                shift = 0;
+                result = 0;
+                do {
+                    b = encoded.charAt(index++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lng += dlng;
+
+                LatLng p = new LatLng((((double) lat / 1E5)),
+                        (((double) lng / 1E5)));
+                poly.add(p);
+            }
+
+            return poly;
+        }
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creamos una conexion http
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Conectamos
+            urlConnection.connect();
+
+            // Leemos desde URL
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e){
+            Log.d("Exception", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
     }
 }
