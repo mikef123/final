526b23e380371d33c13641aa8b9089e6b68a25c8
Miguel Urquijo <miguelferu@gmail.com>
2017-11-21 06:04:03 -0500

    miguelferu@gmail.com
    Version con instagram

---
+++ app/src/main/java/com/valquiria/myapplication/view/fragment/ProfileFragment.java
@@ -0,0 +1,68 @@
+package com.valquiria.myapplication.view.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.valquiria.myapplication.R;
+import com.valquiria.myapplication.adapter.PictureAdapterRecyclerView;
+import com.valquiria.myapplication.model.Picture;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_profile, container, false);
+        showToolbar("",false,view);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureProfileRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buidPictures(), R.layout.cardview_picture, getActivity());
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+        return view;
+    }
+
+    public ArrayList<Picture> buidPictures(){
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg",  "Miguel Urquijo", "4 días", "3 Me gusta"));
+        pictures.add(new Picture("http://www.enjoyart.com/library/landscapes/tuscanlandscapes/large/Tuscan-Bridge--by-Art-Fronckowiak-.jpg", "David Pedraza", "3 días", "1 Me gusta"));
+        pictures.add(new Picture("http://www.educationquizzes.com/library/KS3-Geography/river-1-1.jpg", "Kata Pedraza", "10 días", "50 Me gusta"));
+        return  pictures;
+
+    }
+
+
+    public void showToolbar(String tittle, boolean upButton, View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+
+    }
+}
